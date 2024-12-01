f3e730c1f6de61bd5e406aa02e3bd2af163db652
edaywalid <becharwalid9@gmail.com>
2024-12-01 22:41:59 +0100

    added simple add function

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int add (int x , int y) {
+    return x + y ;
+}
+
 int main()
 {
-
+    int x , y ;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+    printf("Sum of %d and %d is %d\n", x, y, add(x, y));
     return 0;
 }
